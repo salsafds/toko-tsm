1f8223ec5f184cd7d8522cd121a4f4ad5fabe1d1
octavia nur aisya <octavianuraisya@gmail.com>
2025-10-03 15:38:33 +0700

    auth login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // Menampilkan form login
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+// Proses login
+public function login(Request $request)
+{
+    $credentials = $request->validate([
+        'username' => 'required|string',
+        'password' => 'required|string',
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->intended('/dashboardmaster');
+    }
+
+    return back()->withErrors([
+        'login' => 'Username atau password salah.',
+    ])->withInput();
+}
+}
