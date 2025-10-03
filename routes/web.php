45f2241dcc6d36327da52ae25715a947cc9c86da
ismahirdhyaa <151595510+ismahirdhyaa@users.noreply.github.com>
2025-10-03 14:27:06 +0700

    dashboardmaster

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('Signin');
 })->name('login');
+
+Route::get('/dashboardmaster', function () {
+    return view('dashboardmaster'); // nanti kita bikin view dengan nama ini
+})->name('dashboardmaster');
