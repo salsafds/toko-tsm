b34f1d31f922deb03a323b1a2c0563d3160e3bd9
Fadhilah Salsabilah <salsfds11@gmail.com>
2025-10-28 13:51:26 +0700

    + UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,11 +11,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'id_user' => 'USR001',
-            'nama_lengkap' => 'Admin Utama',
+            [
+                'id_user' => 'USR001',
+            'nama_lengkap' => 'Admin Master',
             'alamat_user' => 'Jl. Mawar No. 1',
             'telepon' => '081234567890',
-            'username' => 'admin',
+            'username' => 'adminm',
             'password' => Hash::make('password123'),
             'foto_user' => null,
             'jenis_kelamin' => 'Laki-laki',
@@ -24,9 +25,28 @@ class UserSeeder extends Seeder
             'tanggal_keluar' => null,
             'id_role' => 'R01',         // FK ke role
             'id_jabatan' => 'J01',      // FK ke jabatan
-            'id_pendidikan' => 'PD01',       // FK ke pendidikan (S1)
+            'id_pendidikan' => 'PD03',       // FK ke pendidikan (S1)
             'created_at' => now(),
             'updated_at' => now(),
+            ],
+            [
+            'id_user' => 'USR002',
+            'nama_lengkap' => 'Admin Toko',
+            'alamat_user' => 'Jl. Mawar No. 1',
+            'telepon' => '081234567890',
+            'username' => 'admint',
+            'password' => Hash::make('password123'),
+            'foto_user' => null,
+            'jenis_kelamin' => 'Perempuan',
+            'status' => 'aktif',
+            'tanggal_masuk' => '2024-01-01',
+            'tanggal_keluar' => null,
+            'id_role' => 'R02',         // FK ke role
+            'id_jabatan' => 'J02',      // FK ke jabatan
+            'id_pendidikan' => 'PD02',       // FK ke pendidikan (S1)
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
         ]);
     }
 }
